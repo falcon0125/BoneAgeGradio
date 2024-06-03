7314d60b15857789239a24aafa8b238030b1be5f
falcon0125 <falcon0125@gmail.com>
2024-06-03 23:43:52 +0800

    reduced preprocessing

--- app/app.py
+++ app/app.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 import json
 import tensorflow as tf
+#from tensorflow import keras
 import albumentations as A
 from albumentations import ( 
     HorizontalFlip, CLAHE, GridDistortion, ShiftScaleRotate, Resize, RandomBrightnessContrast
@@ -10,9 +11,9 @@ from albumentations import (
 
 
 transforms = A.Compose([
-    ShiftScaleRotate(always_apply=False, p=1.0, shift_limit=(-0.05, 0.05), scale_limit=(-0.1, 0.2), rotate_limit=(-5, 5), interpolation=1, border_mode=0, value=(0, 0, 0), mask_value=None),
-    RandomBrightnessContrast(brightness_limit=0.2, contrast_limit=0.1),
-    GridDistortion(always_apply=False, p=1.0, num_steps=6, distort_limit=(-0.2, 0.2), interpolation=1, border_mode=0, value=(0, 0, 0), mask_value=None),
+    #ShiftScaleRotate(always_apply=False, p=0.3, shift_limit=(-0.05, 0.05), scale_limit=(-0.1, 0.2), rotate_limit=(-5, 5), interpolation=1, border_mode=0, value=(0, 0, 0), mask_value=None),
+    RandomBrightnessContrast(brightness_limit=0.1, contrast_limit=0.1),
+    #GridDistortion(always_apply=False, p=1.0, num_steps=6, distort_limit=(-0.2, 0.2), interpolation=1, border_mode=0, value=(0, 0, 0), mask_value=None),
     CLAHE(always_apply=False, p=0.5, clip_limit=(1, 4), tile_grid_size=(8, 8)),
     HorizontalFlip(p=0.5),
     Resize(p=1.0, height=256, width=256, interpolation=0),]
@@ -23,8 +24,9 @@ js = json.load(open("atlas.json",'r'))
 """[{"path":"atlas/girl-8m.jpg","name":"girl-8m","gender":"girl","ageo":"8m","age":0.6666666667},"""
 delta = 3
 img_height, img_width = 256,256
-batch_size = 16
-model = tf.keras.models.load_model(r"pruned_bone_age.h5", compile=False)  
+batch_size = 5
+
+model = tf.keras.models.load_model(r"pruned_bone_age.h5", compile=False)
 
 def inference(gender, image_np ):
     sex = 1 if gender == 'boy' else 0    
@@ -36,19 +38,21 @@ def inference(gender, image_np ):
     img_list = ([image] * batch_size)
     imga = np.array(list(map(transform_f,img_list)))
     imga = imga / 256
-    
+   
     predict = model.predict([imga,np.array([sex]*batch_size)], batch_size=batch_size)
     s = pd.Series(predict.reshape(-1))
     argmax, argmin = s.argmax(), s.argmin()
-    del s[argmax]
-    del s[argmin]
-    output = f"predict Bone Age: {s.mean():.1f} +/- {s.std():.1f} y/o\ngender:{gender}"
+    #del s[argmax]
+    #del s[argmin]
+    output = f"Predicted Bone Age: {s.mean():.1f}} y/o\ngender:{gender}"
     df = s.to_frame()
     age_range = s.mean()
     print(output)
     #print(s)
     pathlist = [j['path'] for j in js if 
                 j["age"]>(age_range-delta) and j["age"]<(age_range+delta) and j['gender']==gender]
+    #del model
+    tf.keras.backend.clear_session()
     return output, gr.Gallery(pathlist, label="Atlas", show_download_button=False, show_share_button=False)
 
 demo = gr.Interface(
